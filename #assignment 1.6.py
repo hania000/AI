c7c851b5c1c9cdbec24e472fb308e9cb20efe6e5
hania <haniaabdulrazzaq3@gmail.com>
2025-08-12 17:57:59 +0500

    Delete #assignment 1.6.py

--- #assignment 1.6.py
+++
@@ -1,7 +0,0 @@
-#assignment 1.6
-
-#Calculate square and cube of a number
-
-A =input("Enter number=")
-print("Square of number is:", int(A)*int(A))
-print("Cube of number is:", int(A)*int(A)*int(A))
