18bba4705731621f7727d70e29763fa80124657e
hania <haniaabdulrazzaq3@gmail.com>
2025-08-12 17:59:03 +0500

    Delete assignment 1.3.py

--- assignment 1.3.py
+++
@@ -1,8 +0,0 @@
-#assignment 1.3
-
-#Calcuate Compound Interest
-
-P=input("enter Principle=")
-R=input("enter Rate=")
-T=input("enter Time=")
-print("compound interest is:",int(P)*(int(1)+float(int(R)/100))**int(T)-int(P))
