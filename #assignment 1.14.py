71c5b79f0cf4af9f5161d2167374b3a5d8a15844
hania <haniaabdulrazzaq3@gmail.com>
2025-08-12 17:56:13 +0500

    Delete #assignment 1.14.py

--- #assignment 1.14.py
+++
@@ -1,9 +0,0 @@
-#assignment 1.14
-
-#Calculate percentage of correct answers
-
-T=input("Enter total number of questions=")
-C=input("Enter number of correct questions=")
-
-percentage=(int(C)/int(T))*100
-print("percentage",percentage)
