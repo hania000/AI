2372499a1dd85dee6ac0c8e6422975524a17d393
hania <haniaabdulrazzaq3@gmail.com>
2025-08-12 17:57:29 +0500

    Delete #assignment 1.5.py

--- #assignment 1.5.py
+++
@@ -1,8 +0,0 @@
-#assignment 1.5
-
-#Calculate average of three numbers
-
-A=input("Enter number=")
-B=input("Enter number=")
-C=input("Enter number=")
-print("average of numbers is:",(int(A)+int(B)+int(C))/3)
